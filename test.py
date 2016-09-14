068054971b6b53da4965d640dfe0c27652ade4f9
Manisha <manishapol@gmail.com>
2016-09-14 14:45:36 +1000

    Changes

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from PyhonTestProject import firstElement
 class TestMyFunction(unittest.TestCase):
     def test_first_element(self):
         self.assertEqual(firstElement([1,2,3,4]), 2)
+    def test_first_element_Sucess(self):
+        self.assertEqual(firstElement([1,2,3,4]), 1)
                          
 if __name__ == '__main__':
     import xmlrunner
